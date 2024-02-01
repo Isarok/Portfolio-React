99905c5bd950249eec7f5e90508e780cd11bd4e8
Isarok <isaac.lucerito@gmail.com>
2024-02-01 20:29:45 +0100

    added Routes for the future

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-
+/* import { RouterProvider } from 'react-router-dom' */
+/* import { router } from './router/Router.jsx' */
 
 
 ReactDOM.createRoot(document.getElementById('root')).render( 
+  /* <RouterProvider router={router} /> */
   <React.StrictMode>
     <App />
   </React.StrictMode>,
