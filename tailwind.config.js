42081acfe4790dff42833f68ed6a270093788ad4
Isarok <isaac.lucerito@gmail.com>
2024-02-03 10:13:09 +0100

    tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,42 @@
 import { fontFamily as _fontFamily } from 'tailwindcss/defaultTheme'
 
 export const theme = {
-  extend: {
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+  theme: {
+    fontFamily: {
+      primary: 'Orbitron',
+      secondary: 'Rajdhani',
+      tertiary: 'Aldrich',
+    },
+    container: {
+      padding: {
+        DEFAULT: '15px',
+      },
+    },
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '960px',
+      xl: '1200px',
+    },
+    extend: {
+      colors: {
+        primary: '#0a0a0a',
+        accent:'#B809C3',
+      },
+      backgroundImage: {
+        site:
+        about:  
+        services:
+      }
+    }
+  },
+  plugins: [],
+
+/*   extend: {
     fontFamily: {
       sans: ['Inter var', ..._fontFamily.sans],
     },
-  },
+  }, */
 }
 
